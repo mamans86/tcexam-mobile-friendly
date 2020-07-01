50a4776d0aac4d24d284ade1e44e201011f19a15
mamans86 <48341998+mamans86@users.noreply.github.com>
2020-07-01 21:23:13 +0700

    Add files via upload

---
+++ shared/jscripts/main.js
@@ -0,0 +1,11 @@
+function menuOpen(){
+	document.getElementById('scrollayer').style.display = 'block';
+}
+
+function menuClose(){
+	document.getElementById('scrollayer').style.display = 'none';
+}
+
+function msgClose(){
+	document.getElementById('msg-modal').style.display = 'none';
+}
