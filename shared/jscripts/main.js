0b0f472cda81ddfeae0ab4695e1a99b8e9b16046
mamans86 <48341998+mamans86@users.noreply.github.com>
2020-07-06 12:36:07 +0700

    Update main.js

--- shared/jscripts/main.js
+++ shared/jscripts/main.js
@@ -1,11 +1,26 @@
 function menuOpen(){
-	document.getElementById('scrollayer').style.display = 'block';
+	$("#scrollayer").animate({left:"0"},300);
 }
 
 function menuClose(){
-	document.getElementById('scrollayer').style.display = 'none';
+	$("#scrollayer").animate({left:"-300px"});
 }
 
 function msgClose(){
-	document.getElementById('msg-modal').style.display = 'none';
+	$("#msg-modal").hide();
+}
+
+function qlistOpen(){
+	$(".qlistCont").animate({right:"0px"});
+}
+
+function qlistHide(){
+	$(".qlistCont").animate({right:"-350px"});
+}
+function commentOpen(){
+	$("span.testcomment").animate({top:"0px"});
+}
+
+function commentHide(){
+	$("span.testcomment").animate({top:"-147px"});
 }
